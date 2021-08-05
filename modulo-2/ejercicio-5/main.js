89377b19e8f21ff211f9a5d0e2d3911293f88cc2
MaitaneAbad <maitaneabadconde@gmail.com>
2021-08-05 11:31:29 +0200

    added exercise

---
+++ modulo-2/ejercicio-5/main.js
@@ -0,0 +1,5 @@
+'use strict'
+
+document.querySelector('.title').innetHTML = ' mi contraseña es: **';
+//const titleElement = document.querySelector('.title');
+//titleElement.innetHTML = ' Mi contraseña es: **';
