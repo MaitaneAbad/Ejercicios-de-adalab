0682b120154944c9f8d191e4e7947aa94c927742
MaitaneAbad <maitaneabadconde@gmail.com>
2021-08-05 17:19:55 +0200

    added exercise6

---
+++ modulo-2/ejercicio-6/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/*const articleElement = document.querySelector('.article');
+
+const content = '<h1> lorem ipsum </h1><img src="http://via.placeholder.com/350x150"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>';
+
+articleElement.innetHTML = content;*/
+
+
+const articleElement = document.querySelector('.article');
+
+const content = '<h1> lorem ipsum </h1><img src="http://via.placeholder.com/350x150"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>';
+
+articleElement.innerHTML = content;
