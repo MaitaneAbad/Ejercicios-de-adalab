2c8a09cdee233dc4ca9ab1717f00032cbb052745
MaitaneAbad <maitaneabadconde@gmail.com>
2021-08-06 10:46:58 +0200

    added exercise 2

---
+++ modulo-2/leccion-2/ejercicio-2/main.js
@@ -0,0 +1,4 @@
+'use strict';
+const superwave = document.querySelector('.superwave');/* para ver desde la consola del navegador el elemento que quiero que se resalte o modificar */
+console.log(superwave);
+superwave.classList.add('highlight');/*para añadir listas desde javascript al HTML*/
