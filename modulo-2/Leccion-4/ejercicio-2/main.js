e49ece45ef6fd17a215fa46370b6c97437753bc6
MaitaneAbad <maitaneabadconde@gmail.com>
2021-08-09 16:31:53 +0200

    added exercise 2

---
+++ modulo-2/Leccion-4/ejercicio-2/main.js
@@ -0,0 +1,14 @@
+function math(a, b, c, d){
+    const result = (a + b + c + d)/4;
+    return result;
+}
+
+const total = math(2, 2, 2, 2);
+console.log(total)
+
+
+const total1 = math(9, 5, 7, 2);
+console.log(total1)
+
+const total2 = math(9, 8, 25, 39);
+console.log(total2)
