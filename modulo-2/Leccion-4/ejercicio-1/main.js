dee848787fdf9645999443f288e4829a8d2775bf
MaitaneAbad <maitaneabadconde@gmail.com>
2021-08-09 16:27:08 +0200

    added exercise 1, lesson 4

---
+++ modulo-2/Leccion-4/ejercicio-1/main.js
@@ -0,0 +1,13 @@
+function math(a, b){
+    const result = a * b;
+    return result;
+}
+
+const total = math(2, 3);
+console.log(total)
+
+const total1 = math(5, 5);
+console.log(total1)
+
+const total2 = math(9, 8);
+console.log(total2)
