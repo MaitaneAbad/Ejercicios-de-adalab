6e6b420eb5e16a7a55f5f6181feb1d66f4ad6fca
MaitaneAbad <maitaneabadconde@gmail.com>
2021-08-10 09:02:02 +0200

    added exercise 4

---
+++ modulo-2/Leccion-4/ejercicio-4/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function number(a){
+return (a) ;
+}
+const numResult = number (25);
+if (numResult % 2 === 0)
+    console.log("El número es par");
+else
+    console.log("El número es impar");
