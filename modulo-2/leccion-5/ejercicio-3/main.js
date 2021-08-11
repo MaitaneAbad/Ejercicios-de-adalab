cdaab48dd98426df6624751472a831c59a83af2d
MaitaneAbad <maitaneabadconde@gmail.com>
2021-08-11 07:12:18 +0200

    added exercise 3

--- modulo-2/leccion-5/ejercicio-3/main.js
+++ modulo-2/leccion-5/ejercicio-3/main.js
@@ -1,5 +1,9 @@
-let text = document.querySelector('.text')
+'use strict';
 
-function test( event ) {
-    event.target.style.color = "lorem ipsum";
+const text = document.querySelector('.text');
+
+function addParagraph () {
+    text.innerHTML +=  `<p class="paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem doloribus reprehenderit blanditiis, vero voluptate harum vel voluptatibus sed ullam aperiam officia consequatur perspiciatis expedita praesentium, tenetur voluptates laboriosam eaque odit?</p>`;
 }
+
+text.addEventListener('mouseover', addParagraph);
