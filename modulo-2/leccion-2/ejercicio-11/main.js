f5fa7bb94c4c17f70a70e18ffcd23a73d40c6e40
MaitaneAbad <maitaneabadconde@gmail.com>
2021-08-18 11:03:55 +0200

    adde exercise 11, lesson 2

---
+++ modulo-2/leccion-2/ejercicio-11/main.js
@@ -0,0 +1,12 @@
+2 + 3 +'5'
+"55"
+/*el 2 y el 3 se suman y el 5 se convierte en texto y los concatena */
+'2'+ 3 + 5;
+"235"
+/*el 2 se covierte en texto y por tanto todo lo que vaya detrás tambien y los vuelve a concatenar */
+123
+123
+/*Simplemente muestra los numeros que se escriben ya que no hay ninguna operacion para realizar */
+'123'
+"123"
+/*Se convierte en texto y por ello la consola los muestra con dobles comillas */
