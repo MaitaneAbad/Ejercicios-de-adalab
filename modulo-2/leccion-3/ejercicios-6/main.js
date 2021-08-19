93f61f019361c44541aad8e5aa5d52eb0696cbee
MaitaneAbad <maitaneabadconde@gmail.com>
2021-08-19 19:49:04 +0200

    added exercise 6 lesson 3

--- modulo-2/leccion-3/ejercicios-6/main.js
+++ modulo-2/leccion-3/ejercicios-6/main.js
@@ -1,8 +1,8 @@
 'use strict';
 
-let avocados;
+let avocados =0;
 const avocadoPrice = 1.5;
-const money = 24;
+const money = 13;
 const calc = money / avocadoPrice;
 
 const a = money >= avocadoPrice ? calc : avocados;
