0a235fcbb8ff2dd1032fe708338c95308158a2d9
MaitaneAbad <maitaneabadconde@gmail.com>
2021-08-19 20:44:04 +0200

    modified JS

--- modulo-2/leccion-5/ejercicio-2/main.js
+++ modulo-2/leccion-5/ejercicio-2/main.js
@@ -2,9 +2,12 @@
 
 const name = document.querySelector('.name');
 const button = document.querySelector('.button');
+//const text = document.querySelector('body');
 
 function hi (event){ /*Para que no se recargue la página cuando hay un formulario se pone en el argumento de la funcion (event) y la primera línea de la función (event.preventDefault()) */
     event.preventDefault();
     console.log(`Hola ${name.value}`)
+    
 }
-button.addEventListener("click", hi);
+//button.addEventListener("click", hi);
+
