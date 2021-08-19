d87f656ebce5bab21e973e2b533f344e610f01a3
MaitaneAbad <maitaneabadconde@gmail.com>
2021-08-19 19:59:42 +0200

    added exercise 7, lesson 3

---
+++ modulo-2/leccion-3/ejercicio-7/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+let ingredient = 'Espinacas';
+
+switch (ingredient) {
+  case 'Pollo':
+    console.log('Pollo asado');
+    break;
+  case 'Merluza':
+    console.log('Merluza en salsa verde');
+    break;
+  case 'Espinacas':
+    console.log('Quiché de espinacas');
+    break;
+  default:
+}
