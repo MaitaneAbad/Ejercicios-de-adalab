b27fa4123b69585a35fc5979dec547d2fea9bb73
MaitaneAbad <maitaneabadconde@gmail.com>
2021-08-19 20:45:25 +0200

    modified JS

--- modulo-2/leccion-5/ejercicio-1/main.js
+++ modulo-2/leccion-5/ejercicio-1/main.js
@@ -3,7 +3,8 @@
 const text = document.querySelector('.text')
 const button = document.querySelector('.boton');
 
-function changeText(){
+function changeText(event){
+    event.preventDefault();
     text.innerHTML = ('Mi primer click, ¡ole yo y la mujer que me parió!');
 }
 
