27ce6e4dcd6f070469591d0565c52c9824121fd3
MaitaneAbad <maitaneabadconde@gmail.com>
2021-08-30 19:36:41 +0200

    added exercise 5, lesson 6

---
+++ modulo-2/leccion-6/ejercicio-5/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const input = document.querySelector('.input');
+
+
+function handlerEventListener(event){
+    event.currentTarget.value
+    console.dir(handlerEventListener)
+}
+
+input.addEventListener('change', handlerEventListener)
