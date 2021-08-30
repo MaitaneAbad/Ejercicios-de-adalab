58d8db0c74b7978f210857e245b016e8a2f3deea
MaitaneAbad <maitaneabadconde@gmail.com>
2021-08-30 11:42:56 +0200

    change function JS

--- modulo-2/leccion-5/ejercicio-7/main.js
+++ modulo-2/leccion-5/ejercicio-7/main.js
@@ -4,16 +4,9 @@ const button =document.querySelector('.js-button');
 
 function changeColor (event){
     event.preventDefault();
-    if(button.classList.contains('js-button')){
-        button.classList.remove('js-button');
-        button.classList.add('button');
-    }
-    else if (button.classList.contains('button')){
-        button.classList.remove('button');
-        button.classList.add('js-button');
-    }
+   const select = event.currentTarget;
+
+   select.classList.toggle('button');
 }
 
-document.addEventListener('click', changeColor);
-
-// IMPORTANTE no se pone en este caso el elemento background sino que se hace directamente sobre document
+button.addEventListener('click', changeColor);
