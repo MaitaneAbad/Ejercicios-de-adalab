abe036b7e7593d77bb59cf7070dd6380b8e36f48
MaitaneAbad <maitaneabadconde@gmail.com>
2021-09-07 19:16:05 +0200

    correct exercise

--- modulo-2/leccion-13/ejercicio-2/main.js
+++ modulo-2/leccion-13/ejercicio-2/main.js
@@ -4,6 +4,14 @@ const teacherMarks = document.querySelector('.js-marks');
 
 const names = ['María', 'Lucía', 'Susana', 'Rocío', 'Inmaculada'];
 const helloNames = names.map(function (hello) {
-  return (teacherMarks.innerHTML = 'hola ' + hello);
+  return 'hola ' + hello;
 });
 console.log(helloNames);
+
+for (let eachNumber of helloNames) {
+  const newItem = document.createElement('li');
+  const newContent = document.createTextNode(eachNumber);
+  newItem.appendChild(newContent);
+  const items = document.querySelector('.items');
+  items.appendChild(newItem);
+}
