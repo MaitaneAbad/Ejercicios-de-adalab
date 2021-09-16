ecee14454f41c5c74eb79d709e734e79b003b91d
MaitaneAbad <maitaneabadconde@gmail.com>
2021-09-16 20:10:42 +0200

    added sass to the started kit

--- modulo-3/leccion-1-ejercicio-3/src/components/App.js
+++ modulo-3/leccion-1-ejercicio-3/src/components/App.js
@@ -1,9 +1,35 @@
-import '../styles/App.sass';
+import '../styles/App.scss';
 
 function App() {
   return (
     <div>
-      <h1>Hola mundo</h1>
+      <header className='header'>
+        <h1>Adivina el número</h1>
+      </header>
+      <main className='main js-main'>
+        <form className='form'>
+          <label className='enterName' for='enterName'>
+            Introduce aquí el número:
+            <input
+              className='input js-input'
+              id='num'
+              type='text'
+              name='num'
+              placeholder=' Ej: 58'
+              required
+            />
+          </label>
+          <button className='buttonNum js-button'>Prueba</button>
+        </form>
+        <div>
+          <p className='paragraphTrack js-textTrack'>
+            Pista: Escribe el número y dale a <span>prueba</span>
+          </p>
+          <p className='paragraphNum js-numberAttempts'>
+            Número de intentos: 0
+          </p>
+        </div>
+      </main>
     </div>
   );
 }
